f876a93eccc341fd19829ac254995aa862279605
Rumaisa Ghayas Uddin <barshadsheikh@gmail.com>
2024-12-23 15:51:06 +0500

    updated

---
+++ 11dec.js
@@ -0,0 +1,30 @@
+function AddTwoNumbers() {
+console.log(8+5)    
+}
+AddTwoNumbers();
+function sum(a , b , c) {
+    return a + b + c;
+
+}
+let result = sum(5 , 8 , 9);
+console.log(result);
+
+console.log(sum(7 , 9 , 13));
+console.log(sum(56 , 78 , 5 ));
+function Multiplication( i , j , k){
+    return i * j * k;
+}
+console.log(Multiplication(6 , 7 , 9));
+function subtraction( g , h , k){
+    return g - h - k;
+
+}
+console.log(subtraction(8 , 9 , 7));
+
+function StudentIntro(name){
+    return "hello my name " + "" + "and I am student of IP"
+
+}
+console.log("eshal");
+console.log("laiba");
+console.log("amna");
---
+++ 25nov.js
@@ -0,0 +1,23 @@
+console.log("Bushra Arshad");
+console.log(2 + 2);
+console.log("sum of 2 numbers is:" + (2 + 2));
+var name; //declare a variable
+var name = "Bushra"; // initialize a variable
+var age = 24;
+var address = " a b c north karachi";
+ console.log(name);
+ console.log(age);
+ console.log(address); 
+ console.log(name + " " + age +  " " + address
+  )
+  let fruitName1 = "orange";
+  let fruitName2 = "strawberry";
+  let fruitName3 = "mango";
+  fruitName3 = "banana";
+let fruitName4 = "lychee";
+console.log( fruitName1);
+console.log(fruitName2); 
+console.log(fruitName3); 
+console.log(fruitName4);
+
+
